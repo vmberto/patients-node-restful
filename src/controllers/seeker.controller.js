a5e89d87da94955ed35bcd9b1629ba385387b049
UmbertoB <umbertobarros@mangue3.com>
2019-01-24 09:32:24 -0300

    [master] | refactors

--- src/controllers/seeker.controller.js
+++ src/controllers/seeker.controller.js
@@ -8,17 +8,26 @@ const SeekerController = {
     async getCep(req, res) {
 
         const { cep } = req.params;
+        let responseBundle = {}
 
         https.get(`https://viacep.com.br/ws/${cep}/json/`, (resp) => {
             let data = '';
 
             resp.on('data', (chunk) => {
-                console.log(1);
                 data += chunk;
             });
 
             resp.on('end', () => {
-                res.send(JSON.parse(data));
+                
+                data = JSON.parse(data);
+                
+                if (data.erro) {
+                    responseBundle = { error: true, msg: 'CEP não encontrado'}
+                } else {
+                    responseBundle = data;
+                }
+
+                res.status(200).send(responseBundle);
             });
 
         }).on("error", (err) => {
